121ab0381a02bcd5f66385170cf242d113b239d3
kbesseghir <k.besseghir@esi-sba.dz>
2023-10-19 20:29:49 +0100

    Commit2

--- desturcturing/challenges.js
+++ desturcturing/challenges.js
@@ -3,3 +3,12 @@ let myNumbers = [1, 2, 3, 4, 5];
 // Write Your Destructuring Assignment Here
 [a, ,,,e]= myNumbers;
 console.log(a * e); // 5
+
+
+let mySkills = ["HTML", "CSS", "JavaScript", ["PHP", "Python", ["Django", "Laravel"]]];
+
+// Write Your Destructuring Assignment Here
+[a,b,c,[d,e,[f,g,h]]]=mySkills
+console.log(`My Skills: ${a}, ${b}, ${c}, ${d}, ${e}, ${f}, ${g}`);
+
+// My Skills: HTML, CSS, JavaScript, PHP, Python, Django, Laravel
