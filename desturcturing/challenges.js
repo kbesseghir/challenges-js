ad6cf925e055151dcc4639219ae808c4e2e8c937
kbesseghir <k.besseghir@esi-sba.dz>
2023-10-21 17:22:49 +0100

    Commit3

--- desturcturing/challenges.js
+++ desturcturing/challenges.js
@@ -12,3 +12,17 @@ let mySkills = ["HTML", "CSS", "JavaScript", ["PHP", "Python", ["Django", "Larav
 console.log(`My Skills: ${a}, ${b}, ${c}, ${d}, ${e}, ${f}, ${g}`);
 
 // My Skills: HTML, CSS, JavaScript, PHP, Python, Django, Laravel
+
+
+let arr1 = ["Ahmed", "Sameh", "Sayed"];
+let arr2 = ["Mohamed", "Gamal", "Amir"];
+let arr3 = ["Haytham", "Shady", "Mahmoud"];
+
+// Play With Arrays To Prepare For Destructuring
+// Write Your Destructuring Assignment Here
+let arrTotal = arr1.concat( arr2,arr3);
+[c,,,b,,,,a]=arrTotal
+console.log(arrTotal)
+console.log(`My Best Friends: ${a}, ${b}, ${c}`);
+
+// My Best Friends: Shady, Mahmoud, Ahmed
