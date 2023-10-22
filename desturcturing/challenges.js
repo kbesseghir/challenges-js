b71b50d40a6afe4584c9f7d087bf6ab0c408801b
kbesseghir <k.besseghir@esi-sba.dz>
2023-10-22 13:22:49 +0100

    Commit4

--- desturcturing/challenges.js
+++ desturcturing/challenges.js
@@ -26,3 +26,25 @@ console.log(arrTotal)
 console.log(`My Best Friends: ${a}, ${b}, ${c}`);
 
 // My Best Friends: Shady, Mahmoud, Ahmed
+
+
+
+const member = {
+    age: 30,
+    working: false,
+    country: "Egypt",
+    hobbies: ["Reading", "Swimming", "Programming"],
+  };
+  
+  // Write Your Destructuring Assignment Here
+  ({age:a,working:w}=member)
+  console.log(`My Age Is ${a} And Iam ${w ? "" : "Not"} Working`);
+  // My Age Is 30 And Iam Not Working
+  ({country:c}=member)
+
+  console.log(`I Live in ${c}`);
+  // I Live in Egypt
+  ({hobbies:[h1,,h3]}=member)
+
+  console.log(`My Hobbies: ${h1} And ${h3}`);
+  // My Hobbies: Reading And Programming
