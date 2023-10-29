87855ab3538d2479e492283371e0a3566ef5ac4c
kbesseghir <k.besseghir@esi-sba.dz>
2023-10-29 10:56:49 +0100

    Commit10

--- map/Set/challenges.js
+++ map/Set/challenges.js
@@ -3,3 +3,10 @@ setOfNumbers.add(20);
 setOfNumbers.add(setOfNumbers.size);
 
 console.log(setOfNumbers)
+
+
+
+let myFriends = ["Osama", "Ahmed", "Sayed", "Sayed", "Mahmoud", "Osama"];
+let setE = [...new Set(myFriends)].sort();
+
+console.log(setE)
