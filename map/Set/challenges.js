31a2c9ecc25d2e7e7a82f254d80cdeba08b9e92c
kbesseghir <k.besseghir@esi-sba.dz>
2023-11-01 20:16:29 +0100

    Commit11

--- map/Set/challenges.js
+++ map/Set/challenges.js
@@ -10,3 +10,24 @@ let myFriends = ["Osama", "Ahmed", "Sayed", "Sayed", "Mahmoud", "Osama"];
 let setE = [...new Set(myFriends)].sort();
 
 console.log(setE)
+
+
+let myInfo = {
+    username: "Osama",
+    role: "Admin",
+    country: "Egypt",
+  };
+
+
+  
+ 
+  let myMap = new Map(Object.entries(myInfo));
+  
+
+  console.log(myMap);
+
+  console.log(myMap.size);
+  
+ 
+  console.log(myMap.has('role'));
+  
