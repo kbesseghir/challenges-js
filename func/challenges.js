f8abc09129fa678b267f15f8855a225fbf88261d
kbesseghir <k.besseghir@esi-sba.dz>
2023-11-07 10:43:33 +0100

    commit

--- func/challenges.js
+++ func/challenges.js
@@ -11,3 +11,35 @@ function sayHello(theName, theGender) {
   sayHello("Osama", "Male"); // "Hello Mr Osama"
   sayHello("Eman", "Female"); // "Hello Miss Eman"
   sayHello("Sameh"); // "Hello Sameh"
+
+  //##############################################################
+
+  function calculate(firstNum, secondNum, operation) {
+ if  (secondNum == null) {
+        console.log('error')
+    }
+else if(operation == 'add'){
+    console.log(firstNum + secondNum);
+}  
+ else if(operation == 'subtract'){
+    console.log(firstNum - secondNum);
+}
+else if(operation == 'multiply'){
+    console.log(firstNum * secondNum);
+}
+else if(operation == null){
+    console.log(firstNum + secondNum);
+}
+
+  }
+
+
+
+
+
+  // Needed Output
+  calculate(20); // Second Number Not Found
+  calculate(20, 30); // 50
+  calculate(20, 30, 'add'); // 50
+  calculate(20, 30, 'subtract'); // -10
+  calculate(20, 30, 'multiply'); // 600
